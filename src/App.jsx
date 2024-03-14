782e69e9e2f1bc157a0f356823ed7785fef7d93c
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2024-03-14 18:03:25 -0300

    tarjetas responsivas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Card from './componentes/Card'
+import  Title  from './componentes/Title'
 
 function App() {
 
 
   return (
     <>
+      <Title/>
       <Card />
     </>
   )
---
+++ src/componentes/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Title = () => {
+  return (
+    <>
+    <div>Title</div>
+    </>
+    
+  )
+}
+
+export default Title
